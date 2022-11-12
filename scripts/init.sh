3102d31e550bd0f62e0b049921ef41bafcf66809
LiDong <cnlidong@live.cn>
2022-11-12 22:06:59 +0800

    add pgit

--- scripts/init.sh
+++ scripts/init.sh
@@ -28,12 +28,16 @@ if [ "$REMOTE_CONTAINERS" = "true" ]; then
     npm config set registry https://registry.npmmirror.com
     yarn config set registry https://registry.npmmirror.com
 
+    # 安装pgit
+    echo "Installing pgit"
+    curl -o pgit https://gitee.com/gcslaoli/pgit/raw/main/shell/pgit && chmod +x pgit && sudo mv pgit /usr/local/bin
+
     # 安装cool-tools
     echo "Installing cool-tools ..."
     go install github.com/cool-team-official/cool-admin-go/cool-tools@latest
     # 安装gf
     echo "Installing gf use mirror ..."
-    wget -O gf \
+    pgit wget -O gf \
         https://ghproxy.com/https://github.com/gogf/gf/releases/latest/download/gf_$(go env GOOS)_$(go env GOARCH) &&
         chmod +x gf &&
         ./gf install -y &&
