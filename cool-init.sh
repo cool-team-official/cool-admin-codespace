fb4149de955d1391bf4c70e913ad46b0a4f49cfa
Li Dong <cnlidong@live.cn>
2022-12-01 16:07:52 +0800

    change init to pgit

--- cool-init.sh
+++ cool-init.sh
@@ -3,12 +3,12 @@ set -e
 mkdir cool-team-official
 cd cool-team-official
 echo "开始下载midway后端代码"
-git clone https://github.com/cool-team-official/cool-admin-midway.git 
+pgit clone https://github.com/cool-team-official/cool-admin-midway.git 
 cd cool-admin-midway
 yarn
 cd ..
 echo "开始下载vue前端代码"
-git clone https://github.com/cool-team-official/cool-admin-vue.git 
+pgit clone https://github.com/cool-team-official/cool-admin-vue.git 
 cd cool-admin-vue
 echo "安装前端依赖"
 yarn
